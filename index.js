3c99f17e08944a584aecbae3c7a2989e862ebe3c
sakibmahbub <arafatsakib48@gmail.com>
2023-05-02 15:51:23 +0600

    created recipe data

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 app.use(cors());
-const chefs = require("./data/chef.json");
+const chefs = require("./data/chefs.json");
+const recipe = require("./data/recipe.json");
 
 app.get("/", (req, res) => {
   res.json({ message: "hello server running" });
@@ -10,6 +11,23 @@ app.get("/", (req, res) => {
 
 app.get("/chefs", (req, res) => {
   res.send(chefs);
+});
+
+app.get("/recipe", (req, res) => {
+  res.send(recipe);
+});
+
+app.get("/recipe/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const selectedRecipe = recipe.find((n) => n._id === id);
+  res.send(selectedRecipe);
+});
+
+app.get("/chefs/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const chefsRecipe = recipe.filter((n) => n.chef_id === id);
+  res.send(chefsRecipe);
 });
 
 app.listen(5000, () => {
